e6e4e8d6fafd493aaaed5dd31429a89167e2d034
sqle157 <sql@Sangs-MacBook-Pro.local>
2023-02-10 03:34:31 -0700

    Responsive: make responsive on super small viewport

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
 							className={`block ${
 								visible ? 'flex-1' : 'flex-none'
 							} h-[1px] origin-right bg-gray-600 duration-700 ease-in`}></span>
-						<h2 className='inline-block border-b-2 border-[#00ADB5] text-3xl font-bold uppercase text-[#EEEEEE] md:text-4xl'>
+						<h2 className='inline-block border-b-2 border-[#00ADB5] text-2xl font-bold uppercase text-[#EEEEEE] sm:text-3xl md:text-4xl'>
 							Get In Touch
 						</h2>
 						<span
@@ -37,7 +37,7 @@ const Contact = () => {
 							} h-[1px] origin-right bg-gray-600 duration-700 ease-in`}></span>
 					</div>
 
-					<p className='mx-auto max-w-[700px] text-center text-xl text-[#eeeeee] opacity-75'>
+					<p className='mx-auto max-w-[700px] text-center text-[0.875rem] text-[#eeeeee] opacity-75 sm:text-2xl'>
 						My inbox is always open. Whether you have a question or just want to
 						say hi, I'll try my best to get back to you!{' '}
 					</p>
@@ -47,7 +47,7 @@ const Contact = () => {
 							<a
 								href='https://github.com/sqle157'
 								target='_blank'
-								className='text-md flex items-center gap-3 py-6 text-white focus:outline-gray-600'>
+								className='flex items-center gap-3 py-6 text-white focus:outline-gray-600'>
 								<FaGithub size={32} />
 								https://github.com/sqle157
 							</a>
@@ -73,7 +73,7 @@ const Contact = () => {
 					</div>
 				</div>
 				<Link
-					className='mx-auto grid h-16 w-16 animate-bounce cursor-pointer place-items-center rounded-full bg-[#EEEEEE] text-black'
+					className='mx-auto grid h-[3.25rem] w-[3.25rem] animate-bounce cursor-pointer place-items-center rounded-full bg-[#EEEEEE] text-black sm:h-16 sm:w-16'
 					to='home'
 					href='home'
 					aria-label='back to top button'
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,16 +25,16 @@ const Hero = () => {
 						visible ? 'opacity-100' : 'opacity-0'
 					} delay-100 duration-700 ease-in`}>
 					<div>
-						<p className='text-2xl font-semibold uppercase tracking-wider text-[#EEEEEE] opacity-75'>
+						<p className='text-lg font-semibold uppercase tracking-wider text-[#EEEEEE] opacity-75 sm:text-2xl'>
 							Nice to meet you.
 						</p>
-						<h1 className='py-3 text-5xl font-bold text-[#EEEEEE] md:text-7xl'>
+						<h1 className='py-3 text-3xl font-bold text-[#EEEEEE] sm:text-5xl md:text-7xl'>
 							I'm <span className='text-[#00ADB5]'>Sang</span>,
 						</h1>
-						<h2 className='py-3 text-4xl font-bold uppercase text-[#EEEEEE] md:text-5xl'>
+						<h2 className='py-3 text-2xl font-bold uppercase text-[#EEEEEE] sm:text-4xl md:text-5xl'>
 							A Front-end Web Developer
 						</h2>
-						<p className=' mx-auto py-2 text-[#EEEEEE] opacity-75 sm:max-w-[70%]'>
+						<p className='mx-auto py-2 text-xs text-[#EEEEEE] opacity-75 sm:max-w-[70%] sm:text-xl'>
 							I'm focused on building responsive front-end web applications.
 							With every line of code, I strive to create a clean and beautiful
 							product.
@@ -49,14 +49,14 @@ const Hero = () => {
 								target='_blank'
 								download={download}
 								aria-label={href}
-								className='cursor-pointer rounded-full bg-[#EEEEEE] p-6 text-black shadow-md shadow-gray-400 outline-none duration-300 ease-in focus-within:scale-110 hover:scale-110'>
+								className='cursor-pointer rounded-full bg-[#EEEEEE] p-4 text-black shadow-md shadow-gray-400 outline-none duration-300 ease-in focus-within:scale-110 hover:scale-110 sm:p-6'>
 								{image}
 							</a>
 						))}
 					</div>
 				</div>
 				<Link
-					className={`absolute bottom-3 grid h-16 w-16 animate-bounce cursor-pointer place-items-center rounded-full bg-[#EEEEEE] text-black ${
+					className={`absolute bottom-3 grid h-[3.25rem] w-[3.25rem] animate-bounce cursor-pointer place-items-center rounded-full bg-[#EEEEEE] text-black sm:h-16 sm:w-16 ${
 						visible ? 'opacity-100' : 'opacity-0'
 					} delay-100 duration-700 ease-in`}
 					to='about'
