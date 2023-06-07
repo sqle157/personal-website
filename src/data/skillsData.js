6b938358cf20ac2e0615013d989059ccdec8b816
sqle157 <sql@Sangs-MacBook-Pro.local>
2023-06-06 19:04:53 -0600

    Update skills section

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -9,6 +9,9 @@ import Tailwind from '../assets/icons/tailwind-css.svg';
 import Github from '../assets/icons/github.svg';
 import MongoDB from '../assets/icons/mongodb.svg';
 import Heroku from '../assets/icons/heroku.svg';
+import NextJS from '../assets/icons/nextjs.svg';
+import Sanity from '../assets/icons/sanity.svg';
+import Vercel from '../assets/icons/vercel.svg';
 
 export const skillsData = [
 	{
@@ -41,43 +44,64 @@ export const skillsData = [
 	},
 	{
 		id: 5,
+		image: NextJS,
+		title: 'NextJS',
+		width: 100,
+		height: 100,
+	},
+	{
+		id: 6,
 		image: Typescript,
 		title: 'Typescript',
 		width: 100,
 		height: 100,
 	},
 	{
-		id: 6,
+		id: 7,
 		image: Sass,
 		title: 'Sass',
 		width: 100,
 		height: 100,
 	},
 	{
-		id: 7,
+		id: 8,
 		image: Tailwind,
 		title: 'Tailwind',
 		width: 100,
 		height: 100,
 	},
 	{
-		id: 8,
+		id: 9,
+		image: Sanity,
+		title: 'Sanity',
+		width: 100,
+		height: 100,
+	},
+	{
+		id: 10,
 		image: MongoDB,
 		title: 'MongoDB',
 		width: 100,
 		height: 100,
 	},
 	{
-		id: 9,
+		id: 11,
 		image: Github,
 		title: 'Github',
 		width: 100,
 		height: 100,
 	},
 	{
-		id: 10,
+		id: 12,
 		image: Heroku,
 		title: 'Heroku',
+		width: 100,
+		height: 100,
+	},
+	{
+		id: 13,
+		image: Vercel,
+		title: 'Vercel',
 		width: 100,
 		height: 100,
 	},
